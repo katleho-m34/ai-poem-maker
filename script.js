7c946b96cc3505c797d07b9b9398406285557621
Katleho <katlehom780@gmail.com>
2026-03-27 23:35:40 +0200

    Updated index.html, added CSS blink animation and updated script.js

--- script.js
+++ script.js
@@ -18,6 +18,10 @@ function generatePoem(event) {
     let apiUrl =
       `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+      let poemElement = document.querySelector("#poem");
+      poemElement.classList.remove("hidden");
+      poemElement.innerHTML = `<div class="generating">⌛Generating a poem for you about ${instructionsInput.value}</div>`;
+
       axios.get(apiUrl).then(displayPoem);
 }
 
